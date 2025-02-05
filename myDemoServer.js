53a936349b9fa712cef5d98a39506f98b2182d7a
gkim14 <156844953+gkim14@users.noreply.github.com>
2025-02-04 20:41:37 -0900

    Add files via upload

---
+++ myDemoServer.js
@@ -0,0 +1,67 @@
+//import { MongoClient } from "mongodb";
+const { MongoClient } = require("mongodb");
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 6543;
+const dbName = "myDemoDb"
+const colName = "myDemoCollection"
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/", async(req, res) => {
+  console.log(`${req.body.name}`);
+  myname = `${req.body.name}`;
+  run(myname).catch(console.dir)
+  res.redirect('/');
+});
+
+app.post("/display", async(req,res) => {
+  try {
+    await client.connect();
+    const mydatabase = client.db(dbName);
+    const mycollection = mydatabase.collection(colName);
+    const data = await mycollection.find().toArray();  // Fetch all data from collection
+
+    res.write('<h1> Display Database </h1>')
+    res.write('<ul>');
+    data.forEach(item => {
+        res.write(`<li>${item.name}</li>`);
+    });
+    res.write('</ul>');
+    res.end();  
+  } finally {
+    await client.close();
+  }
+
+});
+
+// Replace the uri string with your MongoDB deployment's connection string.
+const uri = "mongodb://localhost:27017";
+
+const client = new MongoClient(uri);
+
+async function run(myname) {
+  try {
+    await client.connect();
+    const mydatabase = client.db(dbName);
+    const mycollection = mydatabase.collection(colName);
+    // create a document to insert
+    const doc = {
+      name: myname,
+    }
+    const result = await mycollection.insertOne(doc);
+
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+  } finally {
+    await client.close();
+  }
+}
+//run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/myDemoWebpage.html');
+});
+app.listen(port, () => {
+  console.log(`Server running on port${port}`);
+});
